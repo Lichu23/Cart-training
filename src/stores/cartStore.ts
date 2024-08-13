1c8c75d5b78fe08ddccbb0ddafd83c319a7dcfc0
Lichu23 <lisandroxarenax@gmail.com>
2024-08-12 21:47:20 -0300

    added localStorage

--- src/stores/cartStore.ts
+++ src/stores/cartStore.ts
@@ -1,21 +1,33 @@
 import { create } from "zustand";
 import { CartState, Product } from "../types/types";
+import { persist } from "zustand/middleware";
 
-export const useCartStore = create<CartState>((set) => ({
-  cartItems: [],
-  addItem: (product: Product) => {
-    set((state) => {
-      if (state.cartItems.findIndex((item) => item.id === product.id) === -1) {
-        return {
-          cartItems: [...state.cartItems, product],
-        };
-      }
-      return state;
-    });
-  },
-  clearCart: () => {
-    set(() => ({
+const storage = localStorage; // Or use sessionStorage if needed
+
+export const useCartStore = create(
+  persist<CartState>(
+    (set) => ({
       cartItems: [],
-    }));
-  },
-}));
+      addItem: (product: Product) => {
+        set((state) => {
+          if (state.cartItems.findIndex((item) => item.id === product.id) === -1) {
+            return {
+              cartItems: [...state.cartItems, product],
+            };
+          }
+          return state;
+        });
+      },
+      clearCart: () => {
+        set(() => ({
+          cartItems: [],
+        }))
+        // Elimina los datos del localStorage
+        storage.removeItem('cart');
+      },
+    }),
+    {
+      name: 'cart',
+    }
+  )
+);
